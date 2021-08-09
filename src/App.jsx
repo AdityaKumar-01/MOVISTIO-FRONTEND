506ee98f8f09aebf5ff098b33f22eaec19a0c093
aditya <aditya7238195@gmail.com>
2021-08-09 20:02:51 +0530

    folder structure created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
+import HomePage from './Pages/HomePage/HomePage';
 
 const App = () => {
     return (
-        <h1>
-           MOVISTIO 
-        </h1>
+        <HomePage/>
     )
 }
 
---
+++ src/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../../Components/Header/Header.component';
+import "./HomePage.styles.css"
+const HomePage = () => {
+    return (
+        <div className="homepage-container">
+            <Header/>    
+        </div>
+    )
+}
+
+export default HomePage

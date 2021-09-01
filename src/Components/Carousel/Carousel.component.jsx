dcecff8f2a2a0b11cf736907ca4ff49b5ebadc8e
aditya <aditya7238195@gmail.com>
2021-09-01 16:42:16 +0530

    data fetched and put in caraousel card

--- src/Components/Carousel/Carousel.component.jsx
+++ src/Components/Carousel/Carousel.component.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {
+  CarouselProvider,
+  Slider
+} from "pure-react-carousel";
+import "pure-react-carousel/dist/react-carousel.es.css";
+import "./Carousel.syles.css"
+import CarouselCard from './../CarouselCard/CarouselCard.component';
 
 const Carousel = () => {
-   
+  const [ids, setIds] = useState(["19404", "278", "24428"]);
     return (
-        Carousel
+      <CarouselProvider
+        naturalSlideWidth={10}
+        naturalSlideHeight={40}
+        totalSlides={3}
+        interval={5000}
+        isPlaying={true}
+        infinite={true}
+        className="carousel-container"
+      >
+        <Slider>
+          {ids.map(id =>{ return (<CarouselCard id={id}/>)})}
+        </Slider>
+      </CarouselProvider>
+      
     );
 }
 
--- src/Components/CarouselCard/CarouselCard.component.jsx
+++ src/Components/CarouselCard/CarouselCard.component.jsx
@@ -1,11 +1,63 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from "axios";
+import {Slide } from "pure-react-carousel";
+import "pure-react-carousel/dist/react-carousel.es.css";
+import "./CarouselCard.styles.css";
+const CarouselCard = ({id}) => {
 
-const CarouselCard = () => {
-    return (
-        <div>
+    const [data, setData] = useState(null);
+    const getDetails = () =>{
+        var options = {
+          method: "GET",
+          url: `https://api.themoviedb.org/3/movie/${id}?api_key=fc671a9683da1e18060546d03e4e88bd`,
+        };
+        axios
+          .request(options)
+          .then(function (response) {
+            setData(response.data);
+            console.log(response.data);
             
-        </div>
-    )
+          })
+          .catch(function (error) {
+            console.error(error);
+          });
+      }
+    useEffect(() =>{
+       getDetails();
+    },[])
+    return (
+      <Slide>
+        {data ? (<div className="card-container">
+          <div className="card-left-container">
+            <div className="card-metadata">
+              <span>{data.original_language}</span>
+              <span>{data.release_date}</span>
+              <span>{data.vote_average}/10</span>
+              {/* <span>EN</span>
+              <span>1995-09-12</span>
+              <span>12/10</span> */}
+            </div>
+            <div className="card-title">{data.original_title}</div>
+            <div className="card-desc">
+              {data.tagline}
+            </div>
+          </div>
+          <div className="card-right-container">
+            <div className="card-img">
+              <img
+                src={`https://image.tmdb.org/t/p/original${data.poster_path}`}
+                alt="img"
+              />
+              {/* <img
+                src="https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg"
+                alt="img"
+              /> */}
+            </div>
+            <div className="card-genre">{data.genres[0].name}</div>
+          </div>
+        </div>):"null"}
+      </Slide>
+    );
 }
 
 export default CarouselCard

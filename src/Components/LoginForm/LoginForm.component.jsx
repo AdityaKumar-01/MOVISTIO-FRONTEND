10a4836823909047f5b619f85d373daad3192065
aditya <aditya7238195@gmail.com>
2021-10-06 16:20:14 +0530

    :twisted_rightwards_arrows: aditya and master branch  merged

--- src/Components/LoginForm/LoginForm.component.jsx
+++ src/Components/LoginForm/LoginForm.component.jsx
@@ -2,43 +2,45 @@ import React, { useState } from "react";
 import LoginIcon from "@mui/icons-material/Login";
 const LoginForm = () => {
   const [isLogin, setLogin] = useState(true);
-    
+
   const toggleForm = (val) => {
     if (isLogin !== val) setLogin(val);
   };
   return (
-    <div>
-      <span></span>
+    <div className="form-container">
       <div>
-        <span>
-          <span onClick={() => toggleForm(true)}>Login</span>
-          <span onClick={() => toggleForm(false)}>Sign Up</span>
-        </span>
-        {isLogin ? (
-          <div>
-            <span>UserName</span>
-            <input type="text" placeholder="Enter Username" />
-            <span>Password</span>
-            <input type="password" placeholder="Enter Password" />
-            <button>
-              <p>Login</p>
-              <LoginIcon />
-            </button>
-          </div>
-        ) : (
-          <div>
-            <span>UserName</span>
-            <input type="text" placeholder="Enter Username" />
-            <span>Password</span>
-            <input type="password" placeholder="Enter Password" />
-            <span>Confirm Password</span>
-            <input type="password" placeholder="Enter Password Again" />
-            <button>
-              <p>Sign Up</p>
-              <LoginIcon />
-            </button>
-          </div>
-        )}
+        <span></span>
+        <div>
+          <span>
+            <span onClick={() => toggleForm(true)}>Login</span>
+            <span onClick={() => toggleForm(false)}>Sign Up</span>
+          </span>
+          {isLogin ? (
+            <div>
+              <span>UserName</span>
+              <input type="text" placeholder="Enter Username" />
+              <span>Password</span>
+              <input type="password" placeholder="Enter Password" />
+              <button>
+                <p>Login</p>
+                <LoginIcon />
+              </button>
+            </div>
+          ) : (
+            <div>
+              <span>UserName</span>
+              <input type="text" placeholder="Enter Username" />
+              <span>Password</span>
+              <input type="password" placeholder="Enter Password" />
+              <span>Confirm Password</span>
+              <input type="password" placeholder="Enter Password Again" />
+              <button>
+                <p>Sign Up</p>
+                <LoginIcon />
+              </button>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
